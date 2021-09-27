8d20969635a1cb2586258a1233b95dfb6768fa1b
Fenix Sampson <Fenix.sampson@gmail.com>
2021-09-26 23:18:53 -0400

    started working on questions.js file

---
+++ Assets/JS/questions.js
@@ -0,0 +1,86 @@
+
+
+var questions = [
+// question #1
+{
+topic: `Topic: Captain America: The First Avenger`,
+question: `Where does Peggy tell Steve she wants to meet him for a dance before he plunges into the ice?`,
+choice: [`The Cotton Club`, `The Stork Club`, `El Morocco`, `The Copacabana` ],
+answer: `The Stork Club`
+},
+
+// question #2
+{
+    topic: `Topic: Star Trek: The Next Generation`,
+    question: `Where on the Enterprise is Captain Picard when he says the phrase "Make it so" for the first time?`,
+    choice: [`The Battle Bridge`, `Captain's Ready Room`, `Main Bridge`, `Main Shuttle Bay` ],
+    answer: `The Battle Bridge`
+},
+    
+// question #3
+{
+    topic: `Topic: Star Wars: Attack of the Clones`, 
+    question: `The Clone Wars began on which planet?`,
+    choice: [`Tatooine`, `Naboo`, `Geonosis`, `Coruscant` ],
+    answer: `Geonosis`
+},
+    
+// question #4
+{
+    topic: `Topic: Batman`, 
+    question: `Which super villain broke Batman's back, leaving him crippled and wheel-chair bound?`,
+    choice: [`Ra's Al Ghul`, `Killer Croc`, `Joker`, `Bane`],
+    answer: `Bane`
+},
+    
+// question #5
+{
+    topic: `Topic: Stargate: SG-1`, 
+    question: `The Tauri (Humans) eventually become "The Fifth Race". Which of the following are not one of the other four races?`,
+    choice: [`Alterans`, `Furlings`, `Nox`, `Tagreans` ],
+    answer: `Tagreans`
+},
+    
+// question #6
+{
+    topic: `Topic: Farscape`,
+    question: `What is the name of Moya's son?`,
+    choice: [`Talon`, `Rygel`, `Scorpius`, `Braca` ],
+    answer: `Talon`
+},
+    
+// question #7
+{
+    topic: `Topic: Back to the future`, 
+    question: `When Marty travels back in time, he departs from "Twin Pines Mall" in 1985. What mall does he arrive at when he returns back to 1985?`,
+    choice: [`Hill Valley Mall`, `Middleton Mall`, `Twin Pines Mall`, `Lone Pine Mall` ],
+    answer: `Lone Pine Mall`
+},
+    
+// question #8
+{
+    topic: `Topic: Battlestar Galactica`, 
+    question: `Which of the following characters never occupies the office of the President of the Colonies?`,
+    choice: [`Laura Roslin`, `Tom Zarek`, `Lee Adama`, `Gaius Baltar` ],
+    answer: `Tom Zarek`
+},
+    
+// question #9
+{
+    topic: `Topic: Indiana Jones: Raiders of the Lost Ark`, 
+    question: `Who is the main villain in Raiders of the Lost Ark?`,
+    choice: [`Spalko`, `Toht`, `Belloq`, `Donovan` ],
+    answer: `Belloq`
+},
+    
+// question #10
+{
+    topic: `Topic: Firefly/Serenity`,
+    question: `When River's subliminal trigger is activated, what name/words does she say?`,
+    choice: [`I'm a leaf on the wind`, `Cassandra`, `Miranda`, `two by two, hands of blue` ],
+    answer: `Miranda`
+}
+    
+
+
+]
