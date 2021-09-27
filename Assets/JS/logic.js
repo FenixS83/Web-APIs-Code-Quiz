18a62a2216c77cb737f4df6f9f4a0d233a325b87
Fenix Sampson <Fenix.sampson@gmail.com>
2021-09-27 17:35:06 -0400

    started working on logic.js file

---
+++ Assets/JS/logic.js
@@ -0,0 +1,113 @@
+// This is the logic file for pop-culture quiz
+
+
+// Define Variables and arrays
+
+
+
+
+// startquiz function
+
+    // submit function first
+
+    //start function second
+
+
+// Hide Start Screen
+
+    // hide class .quizstart
+
+    // hide .quizstart h1
+
+    // hide .quizstart p
+
+    // hide .disappearingarea
+
+    // hide startfield
+
+    // hide #textareaname
+
+    // hide .start button
+
+
+// show .timerarea
+    // show .timer
+
+
+// show question and answer screen
+
+    // show .questionarea
+
+    // show .questionarea h1
+        // attach topic
+
+    // show .questionarea p
+        // attach questions
+
+    // show .answerfield
+
+    // show .answerbuttons
+        // attach answers
+
+    // receive response
+
+    // check accuracy of response
+
+        // if correct = play sound then go to next question
+
+        // if incorrect = sub. 7 seconds, play sound then go to next question
+
+
+// questions or timer = 0, GAME OVER
+
+
+    // hide question and answer screen
+
+        // hide .timerarea
+            // hide .timer
+
+        // hide .questionarea
+
+        // hide .questionarea h1
+
+        // hide .questionarea p
+        
+        // hide .answerfield
+
+        // hide .answerbuttons
+        
+// store name and final time in local memory
+
+// go to high schores screen
+
+    // show name and final time in ordered list
+
+
+
+
+//addeventlistener list
+
+    // start quiz
+
+    // play again
+
+    // high scores button
+
+    // answer 1
+    
+    // answer 2
+
+    // answer 3
+
+    // answer 4 
+    
+
+
+
+
+
+
+
+
+
+
